cfb96809befcfae48a0bee0f36d814b54355d784
VittorioYan <vittorioyan7@gmail.com>
2019-04-24 17:07:13 +0800

    commit36

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Solution32 solution = new Solution32();
+        Solution36 solution = new Solution36();
         int[] nums = {};
 //        ListNode listNode=new ListNode(1);
 //        listNode.next=new ListNode(2);
@@ -13,7 +13,17 @@ public class Main {
 //        listNode.next.next.next.next=new ListNode(5);
 //        listNode.next.next.next.next.next=new ListNode(6);
         String[] strs ={"aa","aa","aa"};
-        int result=solution.longestValidParentheses("(()");
+        char[][] sudu={
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}};
+        boolean result=solution.isValidSudoku(sudu);
 
 
 
--- src/Solution36.java
+++ src/Solution36.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution36 {
     public boolean isValidSudoku(char[][] board) {
+        List<Character> corresp=new ArrayList<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')continue;
+                if(corresp.contains(board[i][j]))return false;
+                else corresp.add(board[i][j]);
+            }
+            corresp.clear();
+        }
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[j][i]=='.')continue;
+                if(corresp.contains(board[j][i]))return false;
+                else corresp.add(board[j][i]);
+            }
+            corresp.clear();
+        }
 
 
         return true;
